76fefbadc548a498e84feb4d4df817de7bb11193
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2021-11-18 16:56:20 -0500

    bug fixes

--- src/transformers/models/switch/modeling_switch.py
+++ src/transformers/models/switch/modeling_switch.py
@@ -330,7 +330,7 @@ class SwitchLayerFF(nn.Module):
         super().__init__()
 
         self.config = config
-        self.switch_decider = nn.Linear(self.config.d_model, self.config.n_experts)
+        self.router = nn.Linear(self.config.d_model, self.config.n_experts)
         self.softmax = nn.Softmax(dim=-1)
 
         if config.feed_forward_proj == "relu":
@@ -346,8 +346,11 @@ class SwitchLayerFF(nn.Module):
         self.layer_norm = SwitchLayerNorm(config.d_model, eps=config.layer_norm_epsilon)
         self.dropout = nn.Dropout(config.dropout_rate)
 
-    def forward(self, hidden_states: torch.Tensor):
-        forwarded_states = self.layer_norm(hidden_states)
+    def forward(self, gate_inputs: torch.Tensor):
+        # mixed precision
+        gate_inputs = gate_inputs.to(torch.float32)
+        gate_inputs = gate_inputs * torch.FloatTensor(gate_inputs.shape).uniform_(1 - self.epsilon,  1 + self.epsilon)
+        forwarded_states = self.layer_norm(gate_inputs)
         (
             forwarded_states,
             nb_tokens_routed_per_expert,
@@ -355,61 +358,62 @@ class SwitchLayerFF(nn.Module):
             nb_dropped_tokens,
             route_prob_max,
         ) = self._forward_to_experts(forwarded_states)
-        hidden_states = hidden_states + self.dropout(forwarded_states)
+        gate_inputs = gate_inputs + self.dropout(forwarded_states)
         return (
-            hidden_states,
+            gate_inputs,
             nb_tokens_routed_per_expert,
             sum_prob_per_expert,
             nb_dropped_tokens,
             route_prob_max,
         )
 
-    def _forward_to_experts(self, hidden_states: torch.Tensor):
-        batch_size, seq_len, d_model = hidden_states.shape
+    def _forward_to_experts(self, gate_inputs: torch.Tensor):
+        batch_size, seq_len, d_model = gate_inputs.shape
         # print(f"batch size: {batch_size}\nsequence length: {seq_len}\nmodel dimension: {d_model}")
-        hidden_states = hidden_states.view(-1, d_model)
+        gate_inputs = gate_inputs.view(-1, d_model)
         # hidden_states.size = (batch_size * seq_len, d_model)
 
-        route_prob = self.softmax(self.switch_decider(hidden_states))  # routing prob per each token
+        route_prob = self.softmax(self.router(gate_inputs))  # routing prob per each token
         # route_prob.size = (batch_size * seq_len, n_experts)
         route_prob_max, token_routes = torch.max(route_prob, dim=-1)
+        expert_gate, expert_index = torch.topk(route_prob, 1, dim=-1)
+
+        print(">>> Expert_gate shape", expert_gate.shape)
+        print(">>> expert_index shape is", expert_index.shape)
+
+        print(">>> route_prob_max shape is", route_prob_max)
+        print(">>> Token routes shape", token_routes)
+
 
         indexes_list = [
             torch.eq(token_routes, expert_id).nonzero(as_tuple=True)[0] for expert_id in range(self.config.n_experts)
         ]
-        print(">>> Length Hidden states", len(hidden_states))
-        final_output = hidden_states.new_zeros(hidden_states.shape)
-        print(">>> Hidden states shape", hidden_states.shape)
-        print(">>> Capacity Factor", self.config.capacity_factor )
-        print(">>> self.config.n_experts", self.config.n_experts)
-        expert_capacity = int(self.config.capacity_factor * len(hidden_states) / self.config.n_experts)
-        nb_tokens_routed_per_expert = hidden_states.new_tensor(
+        final_output = gate_inputs.new_zeros(gate_inputs.shape)
+        expert_capacity = int(self.config.capacity_factor * len(gate_inputs) / self.config.n_experts)
+        nb_tokens_routed_per_expert = gate_inputs.new_tensor(
             [len(indexes_list[expert_id]) for expert_id in range(self.config.n_experts)]
         )
 
         dropped_tokens = []
-        print(">>> expert_capacity is", expert_capacity)
         
         if self.config.drop_token:
             for expert_id in range(self.config.n_experts):
                 # Need to drop tokens ONLY if the amount of dedicated tokens for an expert is higher than its capacity
-                print("Length index condition", len(indexes_list[expert_id]))
                 if len(indexes_list[expert_id]) > expert_capacity:
                     # Shuffle indexes before dropping
-                    print(">>> Code flow is coming to the place where we append to dropped token")
                     indexes_list[expert_id] = indexes_list[expert_id][torch.randperm(len(indexes_list[expert_id]))]
                     dropped_tokens.append(indexes_list[expert_id][expert_capacity:])
                     indexes_list[expert_id] = indexes_list[expert_id][:expert_capacity]
 
         expert_output = [
-            self.experts[expert_id](hidden_states[indexes_list[expert_id], :])
+            self.experts[expert_id](gate_inputs[indexes_list[expert_id], :])
             for expert_id in range(self.config.n_experts)
         ]
         for expert_id in range(self.config.n_experts):
             final_output[indexes_list[expert_id], :] = expert_output[expert_id]
         if self.config.drop_token and len(dropped_tokens) > 1:
             dropped_tokens = torch.cat(dropped_tokens)
-            final_output[dropped_tokens, :] = hidden_states[dropped_tokens, :]
+            final_output[dropped_tokens, :] = gate_inputs[dropped_tokens, :]
         final_output = final_output * route_prob_max.view(-1, 1)
         final_output = final_output.view(batch_size, seq_len, d_model)
 
