60a7544786e25cebb8e0c3f9c61dc8f430361788
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-12-20 17:51:14 -0500

    fixed switch loss reduction

--- src/transformers/models/must/modeling_must.py
+++ src/transformers/models/must/modeling_must.py
@@ -376,11 +376,19 @@ class MustRouterLayer(nn.Module):
     def compute_load_balancing_loss(self, router_probs, expert_mask):
         # print(f"expert_mask: {expert_mask.shape}")
         # print(f"router_probs: {router_probs.shape}")
-        # Get proportion of tokens routed to each expert, TODO reduce across cores
         n_total_exps = expert_mask.shape[2]
+        if self.config.xla_found:
+            import torch_xla.core.xla_model as xm
+            expert_mask = xm.all_reduce(xm.REDUCE_SUM, expert_mask.float(), scale=1.0 / self.NUM_SHARDS)
         density1 = expert_mask.float().mean(dim=1)
+        if self.config.xla_found:
+            router_probs = xm.all_reduce(xm.REDUCE_SUM, router_probs, scale=1.0 / self.NUM_SHARDS)
         density1_proxy = router_probs.mean(dim=1)
-        loss = (density1 * density1_proxy) * (n_total_exps ** 2)
+        if self.config.xla_found:
+            loss = xm.all_reduce(xm.REDUCE_SUM, (density1 * density1_proxy), scale=1.0 / self.NUM_SHARDS) * (
+                        n_total_exps ** 2)
+        else:
+            loss = (density1 * density1_proxy).sum() * (n_total_exps ** 2)
         return loss
 
     def forward(self, inputs: torch.Tensor):
