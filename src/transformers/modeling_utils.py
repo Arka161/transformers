4698a63c32478d47e3d6940a426ac97c910389b5
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-12-22 22:21:01 -0500

    to bfloat16

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -235,6 +235,8 @@ class ModuleUtilsMixin:
             encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) * -1e4
         elif self.dtype == torch.float32:
             encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) * -1e9
+        elif self.dtype == torch.bfloat16:
+            encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) * -1e9
         else:
             raise ValueError(
                 f"{self.dtype} not recognized. `dtype` should be set to either `torch.float32` or `torch.float16`"
